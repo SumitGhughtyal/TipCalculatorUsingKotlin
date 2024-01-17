92a44dff73eb5949f2a2d65afdab8a424efdde6c
SumitGhughtyal <sumitghughtyal@gmail.com>
2024-01-18 02:20:13 +0530

    first commit

---
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -0,0 +1,121 @@
+package com.example.tiptime
+
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.tiptime.ui.theme.TipTimeTheme
+import java.text.NumberFormat
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TipTimeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    TipTimeLayout()
+                }
+
+            }
+        }
+    }
+}
+
+@Composable
+fun TipTimeLayout() {
+
+    var amountInput by remember { mutableStateOf("")}
+
+    val amount=amountInput.toDoubleOrNull()?:0.0
+    val tip= calculateTip(amount)
+
+    Column (
+        modifier = Modifier
+            .statusBarsPadding()
+            .padding(horizontal = 40.dp)
+            .verticalScroll(rememberScrollState())
+            .safeDrawingPadding(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Text(
+            text = stringResource(id = R.string.calculate_tip),
+            modifier = Modifier
+                .padding(bottom = 16.dp, top = 40.dp)
+                .align(alignment = Alignment.Start))
+
+        EditNumberField(
+            modifier = Modifier
+                .padding(bottom = 32.dp)
+                .fillMaxWidth(),
+            value = amountInput,
+            onValueChanged = {amountInput=it})
+
+        Text(
+            text= stringResource(id = R.string.tip_amount, tip),
+            style=MaterialTheme.typography.displaySmall
+        )
+        Spacer(modifier = Modifier.height(150.dp))
+    }
+
+}
+
+@Composable
+fun EditNumberField(
+    modifier: Modifier,
+    value:String,
+    onValueChanged:(String)->Unit,
+    ){
+    TextField(
+        value = value ,
+        singleLine = true,
+        modifier = modifier,
+        label={Text(stringResource(id = R.string.bill_amount))},
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        onValueChange = onValueChanged)
+}
+
+private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
+    val tip = tipPercent / 100 * amount
+    return NumberFormat.getCurrencyInstance().format(tip)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TipTimeLayoutPreview()
+{
+    TipTimeTheme {
+        TipTimeLayout()
+    }
+}
